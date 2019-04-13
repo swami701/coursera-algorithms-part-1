15becd3e5d8c0ebb01b94be3c5ea78c6e1e0254c
swami701 <swami701search@gmail.com>
2019-04-13 11:09:39 +0800

    THREE SUM: Add Three Sum problem

--- playground/src/ThreeSum.java
+++ playground/src/ThreeSum.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ThreeSum {
+    private static final int[] input = {2, 4, 2, 6, 2};
+    private static final int valToCheck = 14;
+
+    public static void main(String[] args) {
+        System.out.println("Three Sum Program!!");
+        System.out.print("Input Array: ");
+        for (int i : input) {
+            System.out.print("\t" + i);
+        }
+        System.out.println();
+        System.out.println("Is three sum available for " + valToCheck + " : " + isThreeSum(input, valToCheck));
+    }
+
+    private static boolean isThreeSum(int[] arr, int valToCheck) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            int count = 0;
+            if (map.containsKey(i)) {
+                count = map.get(i);
+            }
+            map.put(i, ++count);
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (isAvailableInMap(map, arr[i], arr[j], valToCheck)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isAvailableInMap(Map<Integer, Integer> map, int val1, int val2, int destVal) {
+        int compliment = destVal - (val1 + val2);
+        if (compliment == val1 && val1 == val2) {
+            return map.containsKey(compliment) && map.get(compliment) > 2;
+        } else if (compliment == val1 || compliment == val2) {
+            return map.containsKey(compliment) && map.get(compliment) > 1;
+        } else {
+            return map.containsKey(compliment);
+        }
+    }
 }
